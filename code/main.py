e23c59fc190e1b291d4cb90583de4f103bd3c397
joelpires <joelrogao@hotmail.com>
2020-01-18 14:14:56 -0800

    Support Vector Machines

--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.model_selection import train_test_split
@@ -8,72 +9,84 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+
+
 def pre_processing():
-    base = pd.read_csv('../data/census.csv')
+    print("PRE-PROCESSING THE DATASET...")
+    data = pd.read_csv('../data/census.csv')
 
-    previsores = base.iloc[:, 0:14].values
-    classe = base.iloc[:, 14].values
+    features = data.iloc[:, 0:14].values
+    Y = data.iloc[:, 14].values
 
-    labelencoder_previsores = LabelEncoder()
-    previsores[:, 1] = labelencoder_previsores.fit_transform(previsores[:, 1])
-    previsores[:, 3] = labelencoder_previsores.fit_transform(previsores[:, 3])
-    previsores[:, 5] = labelencoder_previsores.fit_transform(previsores[:, 5])
-    previsores[:, 6] = labelencoder_previsores.fit_transform(previsores[:, 6])
-    previsores[:, 7] = labelencoder_previsores.fit_transform(previsores[:, 7])
-    previsores[:, 8] = labelencoder_previsores.fit_transform(previsores[:, 8])
-    previsores[:, 9] = labelencoder_previsores.fit_transform(previsores[:, 9])
-    previsores[:, 13] = labelencoder_previsores.fit_transform(previsores[:, 13])
+    labelencoder_features = LabelEncoder()
+    features[:, 1] = labelencoder_features.fit_transform(features[:, 1])
+    features[:, 3] = labelencoder_features.fit_transform(features[:, 3])
+    features[:, 5] = labelencoder_features.fit_transform(features[:, 5])
+    features[:, 6] = labelencoder_features.fit_transform(features[:, 6])
+    features[:, 7] = labelencoder_features.fit_transform(features[:, 7])
+    features[:, 8] = labelencoder_features.fit_transform(features[:, 8])
+    features[:, 9] = labelencoder_features.fit_transform(features[:, 9])
+    features[:, 13] = labelencoder_features.fit_transform(features[:, 13])
 
     #onehotencoder = OneHotEncoder(categories='auto')
-    #previsores = onehotencoder.fit_transform(previsores).toarray()
+    #features = onehotencoder.fit_transform(features).toarray()
 
-    #labelencoder_classe = LabelEncoder()
-    #classe = labelencoder_classe.fit_transform(classe)
+    #labelencoder_Y = LabelEncoder()
+    #Y = labelencoder_class.fit_transform(Y)
 
     scaler = StandardScaler()
-    previsores = scaler.fit_transform(previsores)
+    features = scaler.fit_transform(features)
 
-    return (previsores, classe)
-
-
-def train_test_validation(previsores, classe, _test_size=0.15,_random_state=0):
-    previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=_test_size, random_state=_random_state)
-    return (previsores_treinamento, previsores_teste, classe_treinamento, classe_teste)
+    return (features, Y)
 
 
-def classify(classificador, previsores_treinamento, previsores_teste, classe_treinamento):
-
-    if classificador == "Naive Bayes":
-        classificador = GaussianNB()
-    elif classificador == "Decision Tree":
-        classificador = DecisionTreeClassifier(criterion='entropy', random_state=0)
-    elif classificador == "Random Forest":
-        classificador = RandomForestClassifier(n_estimators=40, criterion='entropy', random_state=0)
-    elif classificador == "K-Nearest Neighbour":
-        classificador = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
-    elif classificador == "Logistic Regression":
-        classificador = LogisticRegression()
-
-    classificador.fit(previsores_treinamento, classe_treinamento)
-    previsoes = classificador.predict(previsores_teste)
-    return previsoes
+def train_test_validation(features, Y,  _test_size=0.15,_random_state=0):
+    print("VALIDATION...")
+    X_train, X_test, Y_train, Y_test = train_test_split(features, Y,  test_size=_test_size, random_state=_random_state)
+    return (X_train, X_test, Y_train, Y_test)
 
 
-def evaluate(previsoes, classe_teste):
-    precisao = accuracy_score(classe_teste, previsoes)
-    matriz = confusion_matrix(classe_teste, previsoes)
-    print("Precisao: " + str(precisao))
-    print("Matriz: " + str(matriz))
+def classify(classifier, X_train, X_test, Y_train):
+    print("CLASSIFYING...")
+    if classifier == "Naive Bayes":
+        classifier = GaussianNB()
+    elif classifier == "Decision Tree":
+        classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
+    elif classifier == "Random Forest":
+        classifier = RandomForestClassifier(n_estimators=40, criterion='entropy', random_state=0)
+    elif classifier == "K-Nearest Neighbour":
+        classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+    elif classifier == "Logistic Regression":
+        classifier = LogisticRegression()
+    elif classifier == "Support Vector Machines":
+        classifier = SVC(kernel='linear', random_state=1)
+
+    classifier.fit(X_train, Y_train)
+    predictions = classifier.predict(X_test)
+    return predictions
+
+
+def evaluate(predictions, Y_test):
+    print("EVALUATING...")
+    precision = accuracy_score(Y_test, predictions)
+    matrix = confusion_matrix(Y_test, predictions)
+    print("Precision: " + str(precision))
+    print("Confusion Matrix: ")
+    print(str(matrix))
 
 
 def main():
-    previsores, classe = pre_processing()
-    previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_validation(previsores, classe)
-    classifiers = ["Naive Bayes", "Decision Tree", "Random Forest", "K-Nearest Neighbour", "Logistic Regression"]
+    features, Y = pre_processing()
+    X_train, X_test, Y_train, Y_test = train_test_validation(features, Y)
+    classifiers = ["Naive Bayes", "Decision Tree", "Random Forest", "K-Nearest Neighbour", "Logistic Regression", "Support Vector Machines"]
 
     for classifier in classifiers:
-        previsoes = classify(classifier, previsores_treinamento, previsores_teste, classe_treinamento)
-        evaluate(previsoes, classe_teste)
+        start_time = time.time()
+        print("\n------------------- " + classifier + " ----------------------------")
+        predictions = classify(classifier, X_train, X_test, Y_train)
+        evaluate(predictions, Y_test)
+        print("Time: {:2f} seconds".format((time.time() - start_time)))
 
 
 
